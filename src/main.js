e0c2bddafabb57f0698782fb935265d0311f2f9c
HelloWorld <sc21m2w@leeds.ac.uk>
2022-12-16 21:56:54 +0800

    modified

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+// import App2 from './myTemp.vue';
+// createApp(App2)
 createApp(App).mount('#app')
---
+++ src/myTemp.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<template>
+    <h1>hello world</h1>
+    <!-- <button @click="increment">Count is: {{ count }}</button> -->
+</template>
